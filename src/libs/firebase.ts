dd995972d57fe66d56f6c21b8fe1bad69244b0b2
Matheussgsilva <matheussgsilva@hotmail.com>
2022-01-02 19:14:05 -0300

    Add initial files

---
+++ src/libs/firebase.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC7ocgDb9cWoZutjQoNxMHUyXerrWXKBHI",
+    authDomain: "gallery---react-1feb7.firebaseapp.com",
+    projectId: "gallery---react-1feb7",
+    storageBucket: "gallery---react-1feb7.appspot.com",
+    messagingSenderId: "548181366174",
+    appId: "1:548181366174:web:07fc16f49f247a3df8f28f"
+};
+  
